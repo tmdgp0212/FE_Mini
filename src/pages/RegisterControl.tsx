88f83a6fd724a07d6d94e845a325279e0a96f6de
bwlee <lbw3973@naver.com>
2023-05-04 21:20:27 +0900

    refactor: 회원가입 컴포넌트 정리

--- src/pages/RegisterControl.tsx
+++ src/pages/RegisterControl.tsx
@@ -1,26 +1,12 @@
 import Title from '../components/Title'
-import CheckBox from '../components/CheckBox/CheckBox'
-import RegisterUserTable from '../components/RegisterUserTable/RegisterUserTable'
-import styled from '@emotion/styled'
-import { testUser } from '../components/SearchedTypeTable'
 import AcceptButtons from '../components/AcceptButtons/AcceptButtons'
-
-const TableContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-`
+import UserRegisterControl from '../components/UserRegisterControl'
 
 function RegisterControl() {
   return (
     <>
       <Title text="회원가입 신청 내역" />
-      <CheckBox text="전체 선택" />
-      <TableContainer>
-        {testUser.map((user) => (
-          <RegisterUserTable user={user} />
-        ))}
-      </TableContainer>
+      <UserRegisterControl />
       <AcceptButtons />
     </>
   )
