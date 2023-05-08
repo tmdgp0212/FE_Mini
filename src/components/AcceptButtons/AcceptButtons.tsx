922dce7e13cb62570baa9b158faa18f1ea0a041b
bwlee <lbw3973@naver.com>
2023-05-09 00:15:45 +0900

    feat: AcceptModal 컴포넌트 제작

--- src/components/AcceptButtons/AcceptButtons.tsx
+++ src/components/AcceptButtons/AcceptButtons.tsx
@@ -1,31 +1,58 @@
 import * as S from './style'
 import { useTheme } from '@mui/material'
 import Button from '../Button'
+import AcceptModal from '../AcceptModal'
+import { useState } from 'react'
 
-function AcceptButtons() {
+function AcceptButtons({
+  PositiveMsg,
+  NegativeMsg,
+  acceptFunc,
+  rejectFunc,
+}: {
+  PositiveMsg: string
+  NegativeMsg: string
+  acceptFunc: Function
+  rejectFunc: Function
+}) {
   const theme = useTheme()
-  const handleClick = () => {
-    console.log('handleClick')
+  const [modalMsg, setModalMsg] = useState('')
+  const [open, setOpen] = useState(false)
+  const handleClose = () => setOpen(false)
+  const handleClick = (e: MouseEvent) => {
+    setModalMsg(e.target.id === 'Accept' ? PositiveMsg : NegativeMsg)
+    setOpen(true)
   }
   return (
-    <S.ButtonContainer>
-      <Button
-        bg={theme.app.palette.green1}
-        fontColor={theme.app.palette.white}
-        sx={{ width: '150px' }}
-        onClick={handleClick}
-      >
-        승인
-      </Button>
-      <Button
-        bg={theme.app.palette.red}
-        fontColor={theme.app.palette.white}
-        sx={{ width: '150px' }}
-        onClick={handleClick}
-      >
-        거부
-      </Button>
-    </S.ButtonContainer>
+    <>
+      <S.ButtonContainer>
+        <Button
+          id="Accept"
+          bg={theme.app.palette.green1}
+          fontColor={theme.app.palette.white}
+          sx={{ width: '150px' }}
+          onClick={handleClick}
+        >
+          {PositiveMsg}
+        </Button>
+        <Button
+          id="Reject"
+          bg={theme.app.palette.red}
+          fontColor={theme.app.palette.white}
+          sx={{ width: '150px' }}
+          onClick={handleClick}
+        >
+          {NegativeMsg}
+        </Button>
+      </S.ButtonContainer>
+      <AcceptModal
+        open={open}
+        handleClose={handleClose}
+        modalMsg={modalMsg}
+        acceptFunc={acceptFunc}
+        rejectFunc={rejectFunc}
+      />
+    </>
   )
 }
 
---
+++ src/components/AcceptModal/index.tsx
@@ -0,0 +1,61 @@
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import Modal from '@mui/material/Modal'
+import Button from '../Button'
+import { useTheme } from '@mui/material'
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+}
+export default function AcceptModal({
+  open,
+  handleClose,
+  modalMsg,
+  acceptFunc,
+  rejectFunc,
+}: {
+  open: boolean
+  handleClose: Function
+  modalMsg: string
+  acceptFunc: Function
+  rejectFunc: Function
+}) {
+  const theme = useTheme()
+  const handleClick = () => {
+    modalMsg === '승인' ? acceptFunc() : rejectFunc()
+    handleClose()
+  }
+
+  return (
+    <>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            {modalMsg} 하시겠습니까?
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2, display: 'flex', justifyContent: 'end', gap: '15px' }}>
+            <Button bg={theme.app.palette.green1} fontColor={theme.app.palette.white} onClick={handleClick}>
+              확인
+            </Button>
+            <Button bg={theme.app.palette.red} fontColor={theme.app.palette.white} onClick={handleClose}>
+              취소
+            </Button>
+          </Typography>
+        </Box>
+      </Modal>
+    </>
+  )
+}
