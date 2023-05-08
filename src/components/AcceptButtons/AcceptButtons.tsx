fa7103897bd7704e918cbda1aa8b861081bc010d
LeeByoungWook <lbw3973@naver.com>
2023-05-08 19:51:01 +0900

    feat: 승인/반려 버튼 컴포넌트 이벤트 등록

--- src/components/AcceptButtons/AcceptButtons.tsx
+++ src/components/AcceptButtons/AcceptButtons.tsx
@@ -4,12 +4,25 @@ import Button from '../Button'
 
 function AcceptButtons() {
   const theme = useTheme()
+  const handleClick = () => {
+    console.log('handleClick')
+  }
   return (
     <S.ButtonContainer>
-      <Button bg={theme.app.palette.green1} fontColor={theme.app.palette.white} sx={{ width: '150px' }}>
+      <Button
+        bg={theme.app.palette.green1}
+        fontColor={theme.app.palette.white}
+        sx={{ width: '150px' }}
+        onClick={handleClick}
+      >
         승인
       </Button>
-      <Button bg={theme.app.palette.red} fontColor={theme.app.palette.white} sx={{ width: '150px' }}>
+      <Button
+        bg={theme.app.palette.red}
+        fontColor={theme.app.palette.white}
+        sx={{ width: '150px' }}
+        onClick={handleClick}
+      >
         거부
       </Button>
     </S.ButtonContainer>
--- src/components/AcceptButtons/style.ts
+++ src/components/AcceptButtons/style.ts
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled'
 
 export const ButtonContainer = styled.div`
+  margin-top: 15px;
   display: flex;
   justify-content: end;
   gap: 15px;
