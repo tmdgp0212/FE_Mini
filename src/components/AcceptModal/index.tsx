89ead0520ae042cc0c5e01e786400efe76410f91
LeeByoungWook <lbw3973@naver.com>
2023-05-09 15:18:53 +0900

    feat: type 수정 및 체크 되어있는 유저 가져오기 추가

--- src/components/AcceptModal/index.tsx
+++ src/components/AcceptModal/index.tsx
@@ -3,9 +3,11 @@ import Typography from '@mui/material/Typography'
 import Modal from '@mui/material/Modal'
 import Button from '../Button'
 import { useTheme } from '@mui/material'
+import { testUser } from '../UserScheduleControl'
+import { useToast } from '../../hooks'
 
 const style = {
-  position: 'absolute' as 'absolute',
+  position: 'absolute',
   top: '50%',
   left: '50%',
   transform: 'translate(-50%, -50%)',
@@ -21,17 +23,26 @@ export default function AcceptModal({
   modalMsg,
   acceptFunc,
   rejectFunc,
+  checkItems,
 }: {
   open: boolean
-  handleClose: Function
+  handleClose: () => void
   modalMsg: string
-  acceptFunc: Function
-  rejectFunc: Function
+  acceptFunc: () => void
+  rejectFunc: () => void
+  checkItems: string[]
 }) {
+  const { showToast } = useToast('승인 완료')
   const theme = useTheme()
   const handleClick = () => {
     modalMsg === '승인' ? acceptFunc() : rejectFunc()
     handleClose()
+    showToast()
+  }
+
+  const employeeNumberToUserName = () => {
+    const filterUser = testUser.filter((user) => checkItems.includes(user.employeeNumber))
+    return filterUser.map((el) => el.name)
   }
 
   return (
@@ -45,6 +56,7 @@ export default function AcceptModal({
         <Box sx={style}>
           <Typography id="modal-modal-title" variant="h6" component="h2">
             {modalMsg} 하시겠습니까?
+            <p>{employeeNumberToUserName()}</p>
           </Typography>
           <Typography id="modal-modal-description" sx={{ mt: 2, display: 'flex', justifyContent: 'end', gap: '15px' }}>
             <Button bg={theme.app.palette.green1} fontColor={theme.app.palette.white} onClick={handleClick}>
