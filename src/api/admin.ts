6049102d74dc4e51c65dded680473c71269254fe
bwlee <lbw3973@naver.com>
2023-05-15 22:38:31 +0900

    fix: api url 수정

--- src/api/admin.ts
+++ src/api/admin.ts
@@ -2,49 +2,49 @@ import { instance } from './instance'
 import { SearchUserReq, RoleMutateReq, ModifyUserReq } from './type'
 
 export const getVacation = async () => {
-  const res = await instance.get(`/vacation/list/0`)
+  const res = await instance.get(`/api/v1/vacation/list/0`)
   return res.data
 }
 
 export const getDuty = async () => {
-  const res = await instance.get(`/duty/list/0`)
+  const res = await instance.get(`/api/v1/duty/list/0`)
   return res.data
 }
 
 export const acceptVacation = async (id: string) => {
-  const res = await instance.post(`/vacation/ok/${id}`)
+  const res = await instance.post(`/api/v1/vacation/ok/${id}`)
   return res.data
 }
 
 export const rejectVacation = async (id: string) => {
-  const res = await instance.post(`/vacation/rejected/${id}`)
+  const res = await instance.post(`/api/v1/vacation/rejected/${id}`)
   return res.data
 }
 
 export const acceptDuty = async (id: string) => {
-  const res = await instance.post(`/duty/ok/${id}`)
+  const res = await instance.post(`/api/v1/duty/ok/${id}`)
   return res.data
 }
 
 export const rejectDuty = async (id: string) => {
-  const res = await instance.post(`/duty/rejected/${id}`)
+  const res = await instance.post(`/api/v1/duty/rejected/${id}`)
   return res.data
 }
 
 export const getSignUp = async () => {
-  const res = await instance.get(`/member/admin/deactivation/list`)
+  const res = await instance.get(`/api/v1/member/admin/deactivation/list`)
   return res.data
 }
 
 export const acceptSignUp = async (username: string) => {
   console.log('Accept SignUp : ', username)
-  // const res = await instance.post(`/member/admin/active`, { username, memberStatus: 'ACTIVATION' })
-  // return res.data
+  const res = await instance.post(`/api/v1/member/admin/active`, { username, memberStatus: 'ACTIVATION' })
+  return res.data
 }
 export const rejectSignUp = async (username: string) => {
   console.log('Reject SignUp : ', username)
-  // const res = await instance.post(`/member/admin/active`, { username, memberStatus: 'DEACTIVATION' })
-  // return res.data
+  const res = await instance.post(`/api/v1/member/admin/active`, { username, memberStatus: 'DEACTIVATION' })
+  return res.data
 }
 
 export const changeRole = async (data: RoleMutateReq) => {
