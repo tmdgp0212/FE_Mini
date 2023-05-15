26d01b0006d8585819cb32c85da60d8dc74a61b1
bwlee <lbw3973@naver.com>
2023-05-16 02:41:44 +0900

    feat: type 생성 및 로그 제거

--- src/hooks/useGetDuty.ts
+++ src/hooks/useGetDuty.ts
@@ -2,6 +2,27 @@ import { useQuery } from '@tanstack/react-query'
 import { getDuty } from '../api/admin'
 
 export const useGetDuty = () => {
-  const { data: duty } = useQuery({ queryKey: ['duty'], queryFn: getDuty })
-  return { duty }
+  const { data } = useQuery<any, unknown, DeActivatedDuty, any>({ queryKey: ['duty'], queryFn: getDuty })
+  return { data }
+}
+
+export interface DutyContent {
+  createdAt: Date
+  departmentName: string
+  day: string
+  id: string
+  memberName: string
+  status: string
+  positionName: string
+}
+
+export interface DeActivatedDuty {
+  data: {
+    empty: boolean
+    first: boolean
+    last: boolean
+    number: number
+    numberOfElements: number
+    content: DutyContent[]
+  }
 }
